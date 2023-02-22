5b9e7335b194d42449cb8835934ccde8fc54d14d
Alex Adkins <alex.t.adkins@gmail.com>
2023-02-22 16:05:02 -0500

    initialize complete

---
+++ ChartsVibe/src/main/java/com/chartsvibe/ChartsVibe/ChartsVibeApplication.java
@@ -0,0 +1,13 @@
+package com.chartsvibe.ChartsVibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartsVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartsVibeApplication.class, args);
+	}
+
+}
---
+++ ChartsVibe/src/test/java/com/chartsvibe/ChartsVibe/ChartsVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chartsvibe.ChartsVibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartsVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
